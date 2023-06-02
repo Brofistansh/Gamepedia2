91f082d94c5ccb03e36a6cdc4112a1e4b4d0eaeb
Brofistansh <jaiansh2016@gmail.com>
2023-06-02 22:31:36 +0530

    resource and ore scene are linked and diamond scene is created and ore scene ui completed

---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,12 @@
+
+            //This class is auto-generated, please do not overwrite
+
+            public class VuforiaLicense
+            {
+const string license = "QWJVeno3Ny8vLy8vQUFBQm1UZWd4UGNyWWtrd21BUWdOa1MyVFZlSEptVGUzTUV5OVFGaWdNYVorUE9ISUFtV3BaVjN0TXZJZS9ueU05aUFDNU9HUTErV2pDOWxBK0FnYmNsd3lwZjdkRE1oQ3pMNDloLytUOFArcVlaSGdzV2Vub2pVNXRTemlGTWdmS25MSW1yWnNXaWg2NkVOUzA2bnVVSHFrdmhldzVHNTVqRURvOFl2dW5GTHgyRzZtdW05aG1VWGpKdW54WTZvdFdPNUVYbzZRblphcXlUN0ZJa0ZOblN4K2JpM3ZRM2c0Z3JSV1F0UlhmQ3ZpdGpIQlhvUk1JeW5hZ2M0WmZhdGs5NWxJTlpIWUpwNlowU2RQRjF0bUkyUFpROGdIOEYvSkFLK2tya1dsVCtPaDc1SUVlTUFNYTRZeEZQekFoYzlrUi9HL2J5akFRcjFMSkxEdWdtVUJSeHAybnhvSlFWRktmRTZoSFR6VmdSSC84b2Q=";
+
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
---
+++ Assets/scripts/oreresource.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class oreresource : MonoBehaviour
+{
+
+   public void resources()
+   {
+    SceneManager.LoadScene("resources");
+   }
+   public void ore()
+   {
+        SceneManager.LoadScene("ore");
+
+   }
+   
+}
+
+
+
