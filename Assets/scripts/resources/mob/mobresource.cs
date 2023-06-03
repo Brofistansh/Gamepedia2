4ab9fce3dfb42927eb384773a155eed9b3bc7346
Brofistansh <jaiansh2016@gmail.com>
2023-06-03 10:00:05 +0530

    mobresource script added and mob and resources are linked

---
+++ Assets/scripts/resources/mob/mobresource.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class mobresource : MonoBehaviour
+{
+    public void mob()
+   {
+    SceneManager.LoadScene("mob");
+   }
+   public void resources()
+   {
+        SceneManager.LoadScene("resources");
+
+   }
+}
