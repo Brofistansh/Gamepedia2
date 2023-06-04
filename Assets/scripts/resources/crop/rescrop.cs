7b101453892f82d05b635436df324d62cec7bf8f
Brofistansh <jaiansh2016@gmail.com>
2023-06-04 11:36:34 +0530

    crop scene added and linked with resources and sound effects added

---
+++ Assets/scripts/resources/crop/rescrop.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class rescrop : MonoBehaviour
+{
+            public void crops()
+   {
+    SceneManager.LoadScene("crops");
+   }
+   public void resources()
+   {
+        SceneManager.LoadScene("resources");
+
+   }
+}
